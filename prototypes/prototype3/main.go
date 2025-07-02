9abf31404b0281910d4be317e283c6ff58ca66d7
Alexandra Dill <alexandra.minetree.dill@gmail.com>
2025-07-01 23:54:46 -0500

    write scaffolding for a widget-based UI in prototype 3

--- prototypes/prototype3/main.go
+++ prototypes/prototype3/main.go
@@ -1,13 +1,6 @@
 package main
 
-import (
-	"fmt"
-	// "github.com/go-gl/gl"
-	cl "github.com/go-gl/cl/v1.2/cl"
-	_ "github.com/jgillich/go-opencl/cl"
-	"math/rand"
-	"unsafe"
-)
+import "github.com/veandco/go-sdl2/sdl"
 
 /*
 This will be the third iteration of the fractal viewer.
@@ -42,135 +35,222 @@ I suspect the following design decisions will help:
 
  */
 
-const DataSize = 1024
+type Widget struct {
+	X, Y, W, H int32
+	parent     *Widget
+	children   []*Widget
 
-var KernelSource = `
-__kernel void square(
-	__global float* input,
-	__global float* output,
-	const unsigned int count)
-{
-	int i = get_global_id(0)
-	if (i < count)
-			output[i] = input[i] * input[i]
+	data interface{}
+
+	render      func(*Widget, *ProgramContext)
+	handleEvent func(*Widget, *ProgramContext, *inputStateType)
+	dataIsReady func(*Widget) bool
 }
-` + "\x00" // null terminate the string
+
+type rootWidgetData struct {
+	cachedTexture *sdl.Texture
+}
+
+type ProgramContext struct {
+	window   *sdl.Window
+	renderer *sdl.Renderer
+}
+
+func validateProgramContext(p *ProgramContext) bool {
+	if p.window == nil || p.renderer == nil {
+		return false
+	}
+	return true
+}
+
+const (
+	keyMenu = iota
+	numKeys = iota
+)
+
+const (
+	mouseButtonLeft   = iota
+	mouseButtonRight  = iota
+	mouseButtonMiddle = iota
+	numMouseButtons   = iota
+)
+
+const windowW = 640
+const windowH = 480
+
+type inputStateType struct {
+	keys         [numKeys]bool
+	mouseButtons [numMouseButtons]bool
+}
+
+func validateWidgetAndChildren(w *Widget) bool {
+	// conditions not yet known
+	if w.handleEvent == nil || w.render == nil {
+		return false
+	}
+	for _, child := range w.children {
+		if !validateWidgetAndChildren(child) {
+			return false
+		}
+	}
+	return true
+}
 
 func main() {
-	// i might have to use SDL in OpenGL mode
+	// the OpenCL route i intended to go down is ineffective due to the (apparent) inadequacy of existing Go OpenCL
+	// bindings. so instead I'm going to use either GLSL or a CPU/multi-goroutine based method
 
-	// https://ulhpc-tutorials.readthedocs.io/en/latest/gpu/opencl/
-	// for now, just working with the tutorial verbatim
+	// later, I will implement maybe two methods for calculating Mandelbrot plots: n-bit precision (using GLSL?) and
+	// arbitrary precision (using a library, and calculating plot subsets in a multi-goroutine way)
 
-	data := make([]float32, DataSize)
-	for x := 0; x < len(data); x++ {
-		data[x] = rand.Float32()*99 + 1
-	}
+	// for now:
 
-	// get default device
-	var device cl.DeviceId
-	err := cl.GetDeviceIDs(nil, cl.DEVICE_TYPE_GPU, 1, &device, nil)
-	if err != cl.SUCCESS {
-		panic("failed to get device ids")
-	}
-	var errptr *cl.ErrorCode
-
-	// create computer context
-	context := cl.CreateContext(nil, 1, &device, nil, nil, errptr)
-	// this is supposedly always false? and the conversion is redundant?
-	if errptr != nil && *errptr != cl.SUCCESS {
-		panic("failed to create context")
-	}
-	defer cl.ReleaseContext(context)
-
-	// create a command queue
-	cq := cl.CreateCommandQueue(context, device, 0, errptr)
-	// same issue here. is this an error with the tutorial?
-	if errptr != nil && *errptr != cl.SUCCESS {
-		panic("failed to create command queue")
-	}
-
-	// create program
-	srcptr := cl.Str(KernelSource)
-	program := cl.CreateProgramWithSource(context, 1, &srcptr, nil, errptr)
-	if errptr != nil && *errptr != cl.SUCCESS {
-		panic("failed to create program")
-	}
-	defer cl.ReleaseProgram(program)
-
-	err = cl.BuildProgram(program, 1, &device, nil, nil, nil)
-	if err != cl.SUCCESS {
-		var length uint64
-		buffer := make([]byte, DataSize)
-
-		cl.GetProgramBuildInfo(program, device, cl.PROGRAM_BUILD_LOG, uint64(len(buffer)), unsafe.Pointer(&buffer[0]), &length)
-		panic("failed to build program: \n" + string(buffer[0:length]))
-	}
-
-	// get kernel
-	kernel := cl.CreateKernel(program, cl.Str("square"+"\x00"), errptr)
-	if errptr != nil && *errptr != cl.SUCCESS {
-		panic("failed to create compute kernel")
-	}
-	defer cl.ReleaseKernel(kernel)
-
-	// create buffers (memory for opencl program?)
-	input := cl.CreateBuffer(context, cl.MEM_READ_ONLY, 4*DataSize, nil, errptr)
-	if errptr != nil {
-		panic("failed to create input buffer")
-	}
-	defer cl.ReleaseMemObject(input)
-	// i'm guessing 4 refers to 32-bit values?
-	output := cl.CreateBuffer(context, cl.MEM_WRITE_ONLY, 4*DataSize, nil, errptr)
-	if errptr != nil {
-		panic("failed to create output buffer")
-	}
-
-	// write data
-	err = cl.EnqueueWriteBuffer(cq, input, cl.TRUE, 0, 4*DataSize, unsafe.Pointer(&data[0]), 0, nil, nil)
-	if err != cl.SUCCESS {
-		panic("failed to write data to input array")
-	}
-
-	// set kernel args
-	count := uint32(DataSize)
-	// i'm guessing argIndex 0 is the input buffer, and 8 refers to a 64 bit pointer length?
-	err = cl.SetKernelArg(kernel, 0, 8, unsafe.Pointer(&input))
-	if err != cl.SUCCESS {
-		panic("failed to set kernel arg 0")
-	}
-	err = cl.SetKernelArg(kernel, 1, 8, unsafe.Pointer(&output))
-	if err != cl.SUCCESS {
-		panic("failed to set kernel arg 1")
-	}
-	err = cl.SetKernelArg(kernel, 2, 4, unsafe.Pointer(&count))
-	if err != cl.SUCCESS {
-		panic("failed to set kernel arg 2")
-	}
-
-	// okay, so we query to get the maximum local group size...
-	local := uint64(0)
-	err = cl.GetKernelWorkGroupInfo(kernel, device, cl.KERNEL_WORK_GROUP_SIZE, 8, unsafe.Pointer(&local), nil)
-	if err != cl.SUCCESS {
-		panic("failed to get kernel work group")
-	}
-	// ...then we start a single workgroup and execute the kernel on it (?), hence local == global?
-	global := local
-	err = cl.EnqueueNDRangeKernel(cq, kernel, 1, nil, &global, &local, 0, nil, nil)
-	if err != cl.SUCCESS {
-		panic("failed to execute kernel")
-	}
-
-	// this blocks until all queued commands complete
-	cl.Finish(cq)
-
-	results := make([]float32, DataSize)
-	err = cl.EnqueueReadBuffer(cq, output, cl.TRUE, 0, 4*1024, unsafe.Pointer(&results[0]), 0, nil, nil)
+	err := sdl.Init(sdl.INIT_EVERYTHING)
 	if err != nil {
-		panic("failed to enqueue reading results")
+		panic(err)
+	}
+	defer sdl.Quit()
+	window, renderer, err := sdl.CreateWindowAndRenderer(windowW, windowH, sdl.WINDOW_SHOWN)
+	if err != nil {
+		panic(err)
+	}
+	window.SetTitle("fractal viewer prototype 3!!! :DDD")
+	// setup
+	var rootWidget = Widget{
+		X: 0,
+		Y: 0,
+		W: windowW,
+		H: windowH,
+
+		data: &rootWidgetData{
+			cachedTexture: nil,
+		},
+
+		parent:      nil,
+		children:    []*Widget{},
+		render:      nil,
+		handleEvent: nil,
+		dataIsReady: func(w *Widget) bool {
+			var wData = w.data.(*rootWidgetData)
+			if wData.cachedTexture == nil {
+				return false
+			}
+			return true
+		},
 	}
 
-	fmt.Println(results)
+	rootWidget.render = func(w *Widget, p *ProgramContext) {
+		var tex = w.data.(*rootWidgetData).cachedTexture
+		// on nil texture, panic instead of trying to continue. this probably means something's wrong
+		if tex == nil {
+			panic("failed to render rootWidget texture (nil reference and no fallback)")
+		}
+		// quick assert
+		_, _, texW, texH, err := (*tex).Query()
+		if err != nil {
+			panic("failed to query rootWidget texture: " + err.Error())
+		}
+		if texW != rootWidget.W || texH != rootWidget.H {
+			panic("failed assert: texture size does not match rootWidget size!")
+		}
+		err = p.renderer.Copy(tex, &sdl.Rect{W: w.W, H: w.H}, &sdl.Rect{X: w.X, Y: w.Y, W: w.W, H: w.H})
+		if err != nil {
+			panic("renderer failed to copy texture in rootWidget render: " + err.Error())
+		}
+		p.renderer.Present()
+	}
+	rootWidget.handleEvent = func(w *Widget, p *ProgramContext, state *inputStateType) {
+		// asserts are assumed to pass, but it's possible to change the dimensions of the texture at runtime and I don't like that.
+		// maybe there's a solution somewhere
+		// placeholder
+		data := w.data.(*rootWidgetData)
+		if data.cachedTexture == nil {
+			w.data.(*rootWidgetData).cachedTexture, err = p.renderer.CreateTexture(sdl.PIXELFORMAT_RGBA8888, sdl.TEXTUREACCESS_TARGET, w.W, w.H)
+			err = p.renderer.SetRenderTarget(data.cachedTexture)
+			if err != nil {
+				panic(err)
+			}
+			defer func() {
+				err = p.renderer.SetRenderTarget(nil)
+				if err != nil {
+					panic(err)
+				}
+			}()
+			previousDrawColorR, previousDrawColorG, previousDrawColorB, previousDrawColorA, err := p.renderer.GetDrawColor()
+			err = p.renderer.SetDrawColor(255, 0, 0, 255)
+			if err != nil {
+				panic(err)
+			}
+			defer func() {
+				err := p.renderer.SetDrawColor(previousDrawColorR, previousDrawColorG, previousDrawColorB, previousDrawColorA)
+				if err != nil {
+					return
+				}
+			}()
+			err = p.renderer.Clear()
+			if err != nil {
+				panic(err)
+			}
+		} else {
+			err = p.renderer.Copy(w.data.(*rootWidgetData).cachedTexture, &sdl.Rect{0, 0, w.W, w.H}, &sdl.Rect{w.X, w.Y, w.W, w.H})
+		}
+	}
 
-	// and from here the results should just be correct
+	var programContext = ProgramContext{
+		window:   window,
+		renderer: renderer,
+	}
+
+	if !validateWidgetAndChildren(&rootWidget) {
+		panic("failed assert: rootWidget failed to validate")
+	}
+	if !validateProgramContext(&programContext) {
+		panic("failed assert: program context did not validate")
+	}
+
+	var inputState inputStateType = inputStateType{}
+
+	rootWidget.handleEvent(&rootWidget, &programContext, &inputState)
+	if !rootWidget.dataIsReady(&rootWidget) {
+		panic("failed assert: rootWidget data not ready when needed")
+	}
+	rootWidget.render(&rootWidget, &programContext)
+
+	// main event loop
+	for e := sdl.PollEvent(); true; e = sdl.PollEvent() {
+		if e != nil {
+			handleInputEvent(&inputState, e)
+		}
+	}
+}
+
+// is using the same values that SDL uses a problem?
+func handleInputEvent(inputState *inputStateType, e sdl.Event) {
+	switch e.(type) {
+	case *sdl.KeyboardEvent:
+		var ev = e.(*sdl.KeyboardEvent)
+		var state = false
+		if ev.State == sdl.PRESSED {
+			state = true
+		}
+		if ev.Keysym.Sym == sdl.K_m {
+			if ev.State == sdl.PRESSED {
+				inputState.keys[keyMenu] = state
+			}
+		}
+	case *sdl.MouseButtonEvent:
+		var ev = e.(*sdl.MouseButtonEvent)
+		var state = false
+		if ev.State == sdl.PRESSED {
+			state = true
+		}
+		switch ev.Button {
+		case sdl.BUTTON_LEFT:
+			inputState.mouseButtons[mouseButtonLeft] = state
+		case sdl.BUTTON_RIGHT:
+			inputState.mouseButtons[mouseButtonRight] = state
+		case sdl.BUTTON_MIDDLE:
+			inputState.mouseButtons[mouseButtonMiddle] = state
+		}
+	}
 }
